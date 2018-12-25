c857260bbcefdf74a29414d31b357bcb50edb8a0
riserstechies <risers.techies@gmail.com>
2018-12-25 18:13:40 +0530

    fourth commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 // PHP permanent URL redirection
-header("Location: https://amazon.com", true, 301);
+header("Location: https://www.youtube.com/channel/UCV_HCodkhIx43LGAyPENbzA/videos", true, 301);
 exit();
 ?>
